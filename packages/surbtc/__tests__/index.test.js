7d0344d73295a7dbecaecb5b6118ee7ba380ca31
Patricio López <lopezjuripatricio@gmail.com>
2017-07-17 13:47:45 -0400

    Better SurBTC tests

--- packages/surbtc/__tests__/index.test.js
+++ packages/surbtc/__tests__/index.test.js
@@ -4,7 +4,10 @@ describe("SurBTC", () => {
   it("exists", async () => {
     expect(SurBTC).toBeTruthy();
     const mkt = new SurBTC();
-    const candle = await mkt.getCandle();
+    let candle = null;
+    candle = await mkt.getCandle("BTC", "CLP");
+    expect(candle).toBeTruthy();
+    candle = await mkt.getCandle("ETH", "CLP");
     expect(candle).toBeTruthy();
   });
 });
